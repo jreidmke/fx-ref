52674aa0aa6bde5c1f643fc9d66321cc5968ff46
unknown <jreidmke@gmail.com>
2020-11-19 09:24:45 -0600

    new commenting notes

--- videodemo/effects-ref/src/ProfileViewer.js
+++ videodemo/effects-ref/src/ProfileViewer.js
@@ -3,17 +3,17 @@ import axios from "axios";
 // "https://api.github.com/users/elie"
 
 const ProfileViewer = ({ name = "Elie", color = "purple" }) => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    async function loadProfile() {
-      const res = await axios.get(`https://api.github.com/users/${name}`);
-      setData(res.data.name);
+  const [data, setData] = useState(null);//we have no data at outset. data is set to null
+  useEffect(() => {//cannot make useEffect async. nor the component func
+    async function loadProfile() {//func within useEffect is async
+      const res = await axios.get(`https://api.github.com/users/${name}`);//call made
+      setData(res.data.name);//sets data state to name returned from github call
     }
-    loadProfile();
-  }, [name])
+    loadProfile();//don't forget to call the function you wrote!
+  }, [name]) //<==== this part is really cool. useEffect will run anytime the name prop changes. also! ***IF YOU ARE USING A PROP IN YOUR useEffect, YOU NEED TO PASS IT IN SECOND ARG ARRAY.
 
   return (
-    <h3 style={{ color }}>{data ? data : 'Loading...'}</h3>
+    <h3 style={{ color }}>{data ? data : 'Loading...'}</h3>//returning data if loaded, otherwise display str 'Loading...'
   )
 };
 
