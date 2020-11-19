53f04c648d7bb34b1fe4620c750ba2ab1a6f5e02
unknown <jreidmke@gmail.com>
2020-11-19 09:06:12 -0600

    commented first Timer.js with notes

--- videodemo/effects-ref/src/Timer.js
+++ videodemo/effects-ref/src/Timer.js
@@ -3,9 +3,13 @@ import React, { useState, useEffect } from "react";
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setSeconds(seconds => seconds + 1)
-    }, 1000)
+    const intervalId = setInterval(() => { //interval set
+      setSeconds(seconds => seconds + 1)//increases seconds state
+    }, 1000) //every one second
+
+    /*without using useEffect() hook, the setInterval func will reset
+    every re-render (every time seconds state is set). this will cause
+    a huge mess! useEffect() allows this to persist across re-renderings*/
 
     return () => {
       console.log("CLEANUP FUNCTION!!")
